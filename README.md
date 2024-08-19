9c380ac60550838d3a6c0e7665abe2e4b024af47
achieve_dream <wulei7217@gmail.com>
2024-08-19 14:04:29 +0800

    增加meson构建方式

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 
 ![linguist配置](linguist.png)
 
-## CMake配置
+## 方式一：CMake配置
 
 ```cmake
 # 先清除cmake缓存
@@ -19,6 +19,27 @@ set(I18N
 
 qt_add_translations(TargetName (需要替换) TS_FILES ${I18N})
 
+```
+
+## 方式二： meson配置
+
+```jetbrainsmeson
+#ts = files(
+#    'i18n/zh-CN.ts'
+#)
+#ts_files = qt6.compile_translations(ts_files : ts, build_by_default : true) # 单独编译为qm，加载时需要加载指定加载路径
+
+ts_files = qt6.compile_translations(qresource : 'lang.qrc') # 嵌入到软件中，软件可通过translator.load(":/i18n/zh-CN.qm")来加载
+
+```
+
+```xml
+<!--lang.qrc，meson需要手动编写才能嵌入，注意前缀为空-->
+<RCC>
+    <qresource prefix="">
+        <file>i18n/zh-CN.qm</file>
+    </qresource>
+</RCC>
 ```
 
 ## 编写用于翻译的源代码
--- widget_template/i18n/zh-CN.ts
+++ widget_template/i18n/zh-CN.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../ui/languagetest.ui" line="14"/>
         <source>LanguageTest</source>
-        <translation type="unfinished">语言测试</translation>
+        <translation>语言测试2</translation>
     </message>
     <message>
         <location filename="../ui/languagetest.ui" line="27"/>
@@ -17,6 +17,74 @@
         <location filename="../src/languagetest.cpp" line="14"/>
         <source>label</source>
         <translation>标签</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="25"/>
+        <source>颜色</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="31"/>
+        <source>红</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="38"/>
+        <source>绿</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="45"/>
+        <source>蓝</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="55"/>
+        <source>字体</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="61"/>
+        <source>斜体</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="68"/>
+        <source>下划线</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="75"/>
+        <source>加粗</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="85"/>
+        <source>编辑器</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="94"/>
+        <source>清空</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="114"/>
+        <source>确定</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="121"/>
+        <source>退出</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
