a287c8088fbc558c9ca6718896c391f1cb08c31e
achieve_dream <wulei7217@gmail.com>
2024-08-24 15:34:51 +0800

    fix(i18n): 修正资源文件路径和本地化加载逻辑
    更新了lang.qrc中的资源文件路径，将前缀从空字符串更改为'/'，以确保本地化文件正确嵌入资源中。此外，修改了main.cpp中的本地化加载逻辑，使用QLocale::system()自动检测系统语言环境，并输出加载成功或失败的调试信息。

--- widget_template/main.cpp
+++ widget_template/main.cpp
@@ -9,8 +9,11 @@ int main(int argc, char *argv[]) {
     // https://doc.qt.io/qt-6/qtlinguist-cmake-qt-add-translations.html#embedding-generated-qm-files-in-resources
     // By default, the generated .qm files are embedded in a Qt resource that will be linked into the targets passed
     // with TARGETS. The files in the resource are accessible under the resource prefix "/i18n".
-    if (translator.load(":/i18n/zh-CN.qm")) {
+    if (translator.load(QLocale::system(), "", "", "i18n")) {
+        qDebug() << "load " << translator.filePath() << " success";
         QApplication::installTranslator(&translator);
+    } else {
+        qDebug() << "load " << translator.filePath() << " failed";
     }
     LanguageTest m;
     m.show();
